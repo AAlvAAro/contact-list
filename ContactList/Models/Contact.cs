830a0b85471e5b1d6dfb9b24a074ff8b0c26505e
AAlvAAro <aadelgado@feavaq.net>
2014-09-21 03:23:49 -0500

    first commit

---
+++ ContactList/Models/Contact.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+
+namespace ContactList.Models
+{
+    public class Contact
+    {
+        public int ID { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public string PhoneNumber { get; set; }
+        public string Email { get; set; }
+    }
+
+    public class ContactDBContext : DbContext
+    {
+        public DbSet<Contact> Movies { get; set; }
+    }
+}
