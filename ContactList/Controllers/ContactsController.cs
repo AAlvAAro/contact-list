6c5ab332c0495649e81a788d17563268af5cb43b
AAlvAAro <aadelgado@feavaq.net>
2014-09-22 23:17:08 -0500

    contacts controller + contact model relation

--- ContactList/Controllers/ContactsController.cs
+++ ContactList/Controllers/ContactsController.cs
@@ -1,20 +1,120 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ContactList.Models;
 
 namespace ContactList.Controllers
 {
     public class ContactsController : Controller
     {
+        private ContactDBContext db = new ContactDBContext();
+
         //
         // GET: /Contacts/
 
         public ActionResult Index()
         {
+            return View(db.Movies.ToList());
+        }
+
+        //
+        // GET: /Contacts/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            Contact contact = db.Movies.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        //
+        // GET: /Contacts/Create
+
+        public ActionResult Create()
+        {
             return View();
         }
 
+        //
+        // POST: /Contacts/Create
+
+        [HttpPost]
+        public ActionResult Create(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Movies.Add(contact);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(contact);
+        }
+
+        //
+        // GET: /Contacts/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            Contact contact = db.Movies.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        //
+        // POST: /Contacts/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Contact contact)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(contact).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(contact);
+        }
+
+        //
+        // GET: /Contacts/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            Contact contact = db.Movies.Find(id);
+            if (contact == null)
+            {
+                return HttpNotFound();
+            }
+            return View(contact);
+        }
+
+        //
+        // POST: /Contacts/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Contact contact = db.Movies.Find(id);
+            db.Movies.Remove(contact);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
     }
 }
